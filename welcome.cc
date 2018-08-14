7ee49ff8c2f6ed68c8ef1dc911ba35f8cb0cafc4
Estudiante <Estudiante@10.203.1.177>
2018-08-14 08:37:34 -0500

    shure

---
+++ welcome.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    if (argc > 1) {
+        std::cout << std::endl << "Arguments:" << std::endl;
+        for (int i = 1; i < argc; i++) {
+            std::cout << i << ": " << argv[i] << std::endl;
+        }
+    }
+    
+    return 0;
+}
