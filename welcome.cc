c7d829f4de0d810ced1392291deac83e30de6b41
Estudiante <Estudiante@10.203.1.177>
2018-08-14 08:41:13 -0500

    shure

--- welcome.cc
+++ welcome.cc
@@ -1,12 +1,6 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-    if (argc > 1) {
-        std::cout << std::endl << "Arguments:" << std::endl;
-        for (int i = 1; i < argc; i++) {
-            std::cout << i << ": " << argv[i] << std::endl;
-        }
-    }
-    
+    cout<<"Hola Mundo"<<endl;
     return 0;
 }
